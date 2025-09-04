2e39739ed2d5a31611e59d9ed6b976c8ec74280c
Mason <mason@googleemail.com>
2025-09-03 20:32:35 -0400

    new homework submission

--- ch02_Mason_Bradley.js
+++ ch02_Mason_Bradley.js
@@ -17,14 +17,27 @@ console.log(abc.length);
 // → 3
 Most exercises contain a piece of code that you can modify to solve the exercise. Remember that you can click code blocks to edit them.*/
 
+//Solution
+
 for(let number = "#"; 
   number.length <= 7; 
   number += "#") {
   console.log(number)}
 
+//FizzBuzz
+/*Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
 
+When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).*/
 
+//Solution
 
-
-
+let count = 1;
+for(count <= 100;
+  count += 1
+  if(count == for(let numb = 0;
+    numb <= 100;
+    numb += 3) {console.log(numb)}) {
+      console.log("Fizz")
+    }
+) 
 

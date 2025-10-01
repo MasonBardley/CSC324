63310b4f67f15bdbbcaeec9c2077635916cc1970
Mason <mason@googleemail.com>
2025-09-30 22:39:05 -0400

    Homework Incomplete

--- scratch.js
+++ scratch.js
@@ -14,3 +14,32 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 
 let newArray = arrays.reduce((currChar, nextChar) => currChar.concat(nextChar))
 console.log(newArray)
+
+// 9/30
+
+class Box {
+  constructor(color) {
+    this.color = color
+  }
+}
+
+let redBox = new Box("red")
+console.log(redBox)
+
+
+let myItems = []
+myItems.push(1, 2, 3)
+console.log(myItems)
+
+console.log(myItems.indexOf(2))
+
+
+let myNumbs = []
+myNumbs.push(1, 2, 3, 4, 5)
+console.log(myNumbs.indexOf(6))
+let x = 6
+if(myNumbs.indexOf(x) === -1) {
+    myNumbs.push(x)
+} console.log(myNumbs)
+
+
